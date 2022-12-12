95c88ec5900cbf0733296ba550503397bfdc5810
Winfried Koehler <78253700+wirbel-at-vdr-portal@users.noreply.github.com>
2022-12-12 16:21:57 +0100

    Update fe_delivery_system.h

--- dvb/fe_delivery_system.h
+++ dvb/fe_delivery_system.h
@@ -8,25 +8,25 @@
 
 enum fe_delivery_system {
   SYS_UNDEFINED,             /* Not yet defined.                                */
-  SYS_DVBC_ANNEX_A,          /* Cable TV: DVB-C ITU-T J.83 Annex A (Europe)     */
-  SYS_DVBC_ANNEX_B,          /* Cable TV: DVB-C ITU-T J.83 Annex B (N.America)  */
-  SYS_DVBT,                  /* Terrestrial TV: DVB-T                           */
-  SYS_DSS,                   /* Satellite TV: DSS (not fully supported)         */
-  SYS_DVBS,                  /* Satellite TV: DVB-S                             */
-  SYS_DVBS2,                 /* Satellite TV: DVB-S2 and DVB-S2X                */
-  SYS_DVBH,                  /* Terrestrial TV (mobile): DVB-H (deprecated)     */
-  SYS_ISDBT,                 /* Terrestrial TV: ISDB-T                          */
-  SYS_ISDBS,                 /* Satellite TV: ISDB-S                            */
-  SYS_ISDBC,                 /* Cable TV: ISDB-C (no drivers yet)               */
-  SYS_ATSC,                  /* Terrestrial TV: ATSC (8-VSB)                    */
-  SYS_ATSCMH,                /* Terrestrial TV (mobile): ATSC-M/H               */
-  SYS_DTMB,                  /* Terrestrial TV: DTMB                            */
-  SYS_CMMB,                  /* Terrestrial TV (mobile): CMMB (not fully supp.) */
-  SYS_DAB,                   /* Digital audio: DAB (not fully supported)        */
-  SYS_DVBT2,                 /* Terrestrial TV: DVB-T2                          */
-  SYS_TURBO,                 /* Satellite TV: DVB-S Turbo                       */
-  SYS_DVBC_ANNEX_C,          /* Cable TV: DVB-C ITU-T J.83 Annex C (JP)         */
-  SYS_DVBC2,                 /* Cable TV: DVB-C2                                */
+  SYS_DVBC_ANNEX_A,          /* Cable, DVB-C ITU-T J.83 Annex A (Europe)        */
+  SYS_DVBC_ANNEX_B,          /* Cable, DVB-C ITU-T J.83 Annex B (N.America)     */
+  SYS_DVBT,                  /* Terrestrial, DVB-T                              */
+  SYS_DSS,                   /* Satellite, DSS (not fully supported)            */
+  SYS_DVBS,                  /* Satellite, DVB-S                                */
+  SYS_DVBS2,                 /* Satellite, DVB-S2/S2X                           */
+  SYS_DVBH,                  /* Terrestrial (mobile), DVB-H (deprecated)        */
+  SYS_ISDBT,                 /* Terrestrial, ISDB-T                             */
+  SYS_ISDBS,                 /* Satellite, ISDB-S                               */
+  SYS_ISDBC,                 /* Cable, ISDB-C (no drivers yet)                  */
+  SYS_ATSC,                  /* Terrestrial, ATSC (8-VSB)                       */
+  SYS_ATSCMH,                /* Terrestrial (mobile), ATSC-M/H                  */
+  SYS_DTMB,                  /* Terrestrial, DTMB                               */
+  SYS_CMMB,                  /* Terrestrial (mobile), CMMB (not fully supp.)    */
+  SYS_DAB,                   /* Digital audio broadcasting (not fully supp.)    */
+  SYS_DVBT2,                 /* Terrestrial, DVB-T2                             */
+  SYS_TURBO,                 /* Satellite, DVB-S Turbo                          */
+  SYS_DVBC_ANNEX_C,          /* Cable, DVB-C ITU-T J.83 Annex C (Japan)         */
+  SYS_DVBC2,                 /* Cable, DVB-C2                                   */
 };
 
 /* deprecated: SYS_DVBC_ANNEX_AC */
