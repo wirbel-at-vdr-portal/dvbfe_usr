5cadbaf073c25bd2ea98477fa164e265735a01da
Winfried Koehler <78253700+wirbel-at-vdr-portal@users.noreply.github.com>
2022-12-12 16:17:41 +0100

    Update fe_atscmh.h

--- dvb/fe_atscmh.h
+++ dvb/fe_atscmh.h
@@ -13,7 +13,6 @@
 enum atscmh_sccc_block_mode {
   ATSCMH_SCCC_BLK_SEP  = 0, /* SCCC outer code mode independent for each Group Region (A, B, C, D) */
   ATSCMH_SCCC_BLK_COMB = 1, /* all four Regions have the same SCCC outer code mode.                */
-  ATSCMH_SCCC_BLK_RES  = 2  /* Do not use.                                                         */
 };
 
 
@@ -23,7 +22,6 @@ enum atscmh_sccc_block_mode {
 enum atscmh_sccc_code_mode {
   ATSCMH_SCCC_CODE_HLF = 0, /* The outer code rate of a SCCC Block is 1/2 rate.                    */
   ATSCMH_SCCC_CODE_QTR = 1, /* The outer code rate of a SCCC Block is 1/4 rate.                    */
-  ATSCMH_SCCC_CODE_RES = 2  /* Do not use.                                                         */
 };
 
 
@@ -42,7 +40,6 @@ enum atscmh_rs_frame_ensemble {
 enum atscmh_rs_frame_mode {
   ATSCMH_RSFRAME_PRI_ONLY = 0, /* Single: There is only a primary RS Frame for all Group Regions.     */
   ATSCMH_RSFRAME_PRI_SEC  = 1, /* Dual: Primary RS Frame for Region A and B, Secondary for C and D.   */
-  ATSCMH_RSFRAME_RES      = 2  /* Do not use.                                                         */ 
 };
 
 
@@ -53,5 +50,4 @@ enum atscmh_rs_code_mode {
   ATSCMH_RSCODE_211_187 = 0,  /* RS code (211,187) */
   ATSCMH_RSCODE_223_187 = 1,  /* RS code (223,187) */
   ATSCMH_RSCODE_235_187 = 2,  /* RS code (235,187) */
-  ATSCMH_RSCODE_RES     = 3   /* Do not use.       */
 };
